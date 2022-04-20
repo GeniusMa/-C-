c046b611e4319f4f616ef5912de96b04ad100b6c
清禾同学 <87123120+GeniusMa@users.noreply.github.com>
2022-04-20 18:08:43 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// int main()
+// {
+// 	cout<<10%3<<"\n"<<ends;
+// 	return 0;
+// }
+
+// int main()
+// {
+// 	int a=10;
+// 	int b=a++*10;
+// 	cout<<a<<"\n"<<ends;
+// 	cout<<b<<"\n"<<ends;
+
+// 	int a1=10;
+// 	int b1=++a1*10;
+// 	cout<<a1<<"\n"<<ends;
+// 	cout<<b1<<"\n"<<ends;
+// 	return 0;
+// }
+
+// int main()
+// {
+// 	/*
+// 	！非
+// 	&& 与
+// 	|| 或
+// 	*/
+// 	int a,b;
+// 	cout<<"请输入AB！\n"<<ends;
+// 	cin>>a>>b;
+// 	if(a&&b)
+// 		cout<<"True!\n"<<ends;
+// 	else
+// 		cout<<"False!\n"<<ends;
+// 	return 0;
+// }
+
+// int main()
+// {
+// 	int a,b,c;
+// 	cin>>a>>b>>c;
+// 	if(a>b&&a>c)
+// 		cout<<"a"<<ends;
+// 	else if(b>c&&b>a)
+// 		cout<<"b"<<ends;
+// 	else
+// 		cout<<"c"<<ends;
+// 	return 0;
+// }
+
+// int main()
+// {
+// 	int a=30,b=20,c;
+// 	c=(a>b?a:b);
+// 	cout<<"C="<<c<<"\n"<<ends;
+// 	return 0;
+// }
+
+// int main()
+// {
+// 	int a=30,b=40;
+// 	(a>b?a:b)=100;
+// 	cout<<"a="<<a<<ends;
+// 	cout<<"b="<<b<<ends;
+// 	return 0;
+// }
+
+int main()
+{
+	
+	return 0;
+}
